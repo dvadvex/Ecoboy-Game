592a7525be0df8109e7edfcfedc555ee7be1ba79
aldialml <davidrusukatai@gmail.com>
2025-12-06 22:46:14 +0200

    be

--- js/main.js
+++ js/main.js
@@ -13,7 +13,20 @@ document.getElementById("b4").textContent = questions[0].answers[3]
 
 let correctAnswer = questions[0].correct
 
-function checkAnswer(ans){
+function changeQA(){
+    document.getElementById("bober").textContent = questions[1].question
+
+    document.getElementById("b1").textContent = questions[1].answers[0]
+    document.getElementById("b2").textContent = questions[1].answers[1]
+    document.getElementById("b3").textContent = questions[1].answers[2]
+    document.getElementById("b4").textContent = questions[1].answers[3]
+
+    document.getElementById("output").textContent = ""
+
+    correctAnswer = questions[1].correct
+}
+
+async function checkAnswer(ans){
     if(ans == correctAnswer){
         document.getElementById("output").textContent = "Correct!"
         stats[0].money += 67
@@ -21,6 +34,9 @@ function checkAnswer(ans){
         document.getElementById("output").textContent = "Skill issue!"
         stats[0].money -= 67
     }
+
+    await sleep(2000)
+    changeQA()
 }
 
 document.getElementById("b1").addEventListener("click", () => checkAnswer(0))
